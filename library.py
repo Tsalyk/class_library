949c32e61ccce9c45d9db7e8df3a5ed247a75308
Tsalyk <zalukm1@gmail.com>
2021-02-16 16:28:39 +0200

    finished

--- library.py
+++ library.py
@@ -1,3 +1,23 @@
+class Book:
+
+    def __init__(self, book_id, title, author, year, reader_id):
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+        self.year = year
+        self.reader_id = reader_id
+
+
+class People:
+
+    def __init__(self, name, surname, age, books, my_id):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.books = books
+        self.id = my_id
+
+
 class Library:
 
     def __init__(self, list_books, list_readers):
@@ -5,77 +25,62 @@ class Library:
         self.readers = list_readers
 
 
-    def add_book(self, book):
-        self.books.append(book)
+    def add_book(self, book_id, title, author, year, reader_id):
+        self.books.append(Book(book_id, title, author, year, reader_id))
 
 
-    def remove_book(self, book):
-        self.books.remove(book)
+    def remove_book(self, book_id):
+        self.books = list(filter(lambda el: el.book_id != book_id, self.books))
 
 
-    def give_book(self, book, reader):
-        for index, el in enumerate(self.readers):
-            if el[0] == reader:
-                ind = index
+    def give_book(self, book_id, reader_id):
+        for ind, el in enumerate(self.books):
+            if el.book_id == book_id:
+                self.books[ind].reader_id = reader_id
 
-        self.remove_book(book)
-        self.readers[ind] = self.readers[ind], book
+                for subind, subel in enumerate(self.readers):
+                    if subel.id == reader_id:
+                        if self.readers[subind].books != None:
+                            self.readers[subind].books.append(el.title)
+                        else:
+                            self.readers[subind].books = [el.title]
 
 
-    def get_book(self, book, reader):
-        for index, el in enumerate(self.readers):
-            if el[0] == reader:
-                ind = index
+    def get_book(self, book_id, reader_id):
+        for ind, el in enumerate(self.books):
+            if el.book_id == book_id:
+                self.books[ind].reader_id = None
 
-        self.books.append(book)
-        self.readers[ind] = self.readers[ind][0]
+                for subind, subel in enumerate(self.readers):
+                        if subel.id == reader_id:
+                            self.readers[subind].books.remove(el.title)
+
+    def all_books(self):
+        return list(map(lambda el: el.title, self.books))
 
 
     def available_books(self):
-        print(self.books)
+        available_books =  list(filter(lambda el: el.reader_id == None,
+                                        self.books))
+        return list(map(lambda el: el.title, available_books))
 
 
     def not_available_books(self):
-        self.not_availabale_lst = list(filter(lambda el: len(el) == 2,
-                                              self.readers))
-        self.not_availabale_lst = list(map(lambda el: el[1],
-                                           self.not_availabale_lst))
-
-        print(self.not_availabale_lst)
+        available_books =  list(filter(lambda el: el.reader_id != None,
+                                        self.books))
+        return list(map(lambda el: el.title, available_books))
 
 
-    def sort_books_by_title(self, books):
-        pass
+    def sort_books_by_title(self):
+        sorted_books = sorted(self.books, key=lambda el: el.title)
+        return list(map(lambda el: el.title, sorted_books))
 
 
-    def sort_books_by_author(self, books):
-        pass
+    def sort_books_by_author(self):
+        sorted_books = sorted(self.books, key=lambda el: el.author)
+        return list(map(lambda el: el.title, sorted_books))
 
 
-    def sort_books_by_year(self, books):
-        pass
-
-
-class Books:
-
-    def __init__(self, book_id, book_title, book_author, book_year):
-        self.id = book_id
-        self.title = book_title
-        self.author = book_author
-        self.year = book_year
-
-
-
-
-
-lib = Library(["Harry Potter", "Snow"], ["Mark"])
-
-lib.give_book("Harry Potter", "Mark")
-lib.not_available_books()
-print(lib.readers)
-lib.get_book("Harry Potter", "Mark")
-lib.available_books()
-lib.give_book("Snow", "Mark")
-print(lib.readers)
-lib.give_book("Harry Potter", "Mark")
-lib.available_books()
+    def sort_books_by_year(self):
+        sorted_books = sorted(self.books, key=lambda el: el.year)
+        return list(map(lambda el: el.title, sorted_books))
